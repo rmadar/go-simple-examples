f1a559f5de3e7b8c5d0a8cbdb9984ac8d364c7c9
Sebastien Binet <binet@cern.ch>
2020-03-20 11:40:20 +0100

    reading-root-ttree: add error handling, fix scanner code

--- reading-root-ttree/main.go
+++ reading-root-ttree/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"go-hep.org/x/hep/groot"
-	"go-hep.org/x/hep/groot/riofs"
 	"go-hep.org/x/hep/groot/rtree"
 )
 
@@ -15,16 +15,16 @@ func main() {
 		tname  = "test"
 		evtmax = int64(10000)
 	)
-	
+
 	eventLoop(fname, tname, evtmax)
 }
 
 type Event struct {
-	t    Particles
-	b    Particles
-	W    Particles
-	l    Particles
-	v    Particles
+	t Particles
+	b Particles
+	W Particles
+	l Particles
+	v Particles
 }
 
 type Particles struct {
@@ -46,23 +46,36 @@ func eventLoop(fname string, tname string, evtmax int64) {
 	// Event and variables to load
 	var e Event
 	vars := []rtree.ScanVar{
-		{Name: "t_pt" , Value: &e.t.pt},
+		{Name: "t_pt", Value: &e.t.pt},
 		{Name: "t_eta", Value: &e.t.eta},
 		{Name: "t_phi", Value: &e.t.phi},
 		{Name: "t_pid", Value: &e.t.pid},
+		{Name: "b_pt", Value: &e.b.pt},
+		{Name: "b_eta", Value: &e.b.eta},
+		{Name: "b_phi", Value: &e.b.phi},
+		{Name: "b_pid", Value: &e.b.pid},
 	}
 
 	// Create a scanner to perform the event loop
-	sc, _ := rtree.NewScannerVars(tree, vars...)
-	
+	sc, err := rtree.NewScannerVars(tree, vars...)
+	if err != nil {
+		log.Fatalf("could not create scanner: %+v", err)
+	}
+	defer sc.Close()
+
 	// Actual event loop
 	for sc.Next() && sc.Entry() < evtmax {
 
 		// Load the event variables
 		iev := sc.Entry()
 
+		err := sc.Scan()
+		if err != nil {
+			log.Fatalf("could not scan entry %d: %+v", iev, err)
+		}
+
 		// Print
-		if iev%1000==0 {
+		if iev%1000 == 0 {
 			fmt.Println("Evt:", iev)
 			printEvent(e)
 		}
@@ -70,7 +83,7 @@ func eventLoop(fname string, tname string, evtmax int64) {
 }
 
 // Event printing
-func printEvent(e Event){
+func printEvent(e Event) {
 	fmt.Println(" * Top quarks")
 	fmt.Println("   - pT : ", e.t.pt)
 	fmt.Println("   - Eta: ", e.t.eta)
@@ -84,14 +97,19 @@ func printEvent(e Event){
 }
 
 // Helper to open a ROOT file
-func openRootFile(fname string) (*riofs.File) {
-	f, _ := groot.Open(fname)
+func openRootFile(fname string) *groot.File {
+	f, err := groot.Open(fname)
+	if err != nil {
+		log.Fatalf("could not open ROOT file: %+v", err)
+	}
 	return f
 }
 
 // Helper to get a TTree
-func getTtree(f *riofs.File, tname string) (rtree.Tree){
-	obj, _ := f.Get(tname)
+func getTtree(f *groot.File, tname string) rtree.Tree {
+	obj, err := f.Get(tname)
+	if err != nil {
+		log.Fatalf("could not retrieve tree %q: %+v", tname, err)
+	}
 	return obj.(rtree.Tree)
 }
-
