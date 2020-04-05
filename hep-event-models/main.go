194017f62d005e010efabeb8b9e0d9e7ff5793a3
rmadar <romain.madar@gmail.com>
2020-04-05 22:09:48 +0200

    Minor update

--- hep-event-models/main.go
+++ hep-event-models/main.go
@@ -1,31 +1,7 @@
-
-// Exemple of how to use go interface to define several HEP
-// event models. The goal is to have a single event model
-// at the user level, and handle the differences & conversion
-// in one package using interfaces. This example is based on
-// ROOT object translated in go in groot (go-hep package),
-// but can be easily generalized.
-//
-// The use case in mind for this example is to have two inputs
-// formats (say 'EventInFlat' and 'EventInArrays') for one unique
-// output format (say 'EventOut'). The user would have to define
-// once. The format of 'EventInFlat' and 'EventInFlat', and how to
-// translate each of them into 'EventOut'. Then, with a simple flag
-// (passed as argument) one can run the same executable on the two
-// input format. In summary, we have:
-//  1. EventOut, to be processed (add new variables) and saved
-//  2. EventInFlat, to be loaded and converted into EventOut
-//  3. EventInArray, to be loaded and converted into EventOut
-// where the two last are satisfying the single interface 'EventIn'.
-//
-// For this example, we consider events with 2-jets in the final
-// state, described by 12 numbers, 2 x (4-vectors + number of
-// tracks + EM fraction), that can be organized in 3 different ways.
-
+// Manipulate two different structures of input events using go interface 
 package main
 
 import (
-	_ "fmt"
 	"math"
 	"flag"
 	"strings"
