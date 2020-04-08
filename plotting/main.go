3b5e05239f7b80ce914cc956bb61453ea8296e92
rmadar <romain.madar@gmail.com>
2020-04-08 11:04:03 +0200

    Refine plot tuning, factorize object style in different function

--- plotting/main.go
+++ plotting/main.go
@@ -25,7 +25,7 @@ import (
 )
 
 
-var defaultBlack color.NRGBA = color.NRGBA{R: 40, G: 40, B: 40, A: 240}
+var defaultBlack color.NRGBA = color.NRGBA{R: 30, G: 30, B: 30, A: 255}
 
 func main() {
 
@@ -39,23 +39,99 @@ func main() {
 // Use hplot package
 func plotHplot_1D(){
 
-	const npoints = 500
+	// Get (fake) data, simulation and theoretical function
+	histData, histMC, modelFunc := getData()
+	
+	// Make a plot and ask to compile .tex on-the-fly
+	p := hplot.New()
+	p.Title.Text = "\\textbf{APLAS} Dummy -- $\\sqrt{s}=13\\,$TeV $\\mathcal{L}\\,=\\,3\\,$ab\\textsuperscript{-1}"
+	p.Latex = hplot.PDFLatexHandler
+	p.X.Label.Text = "$m_{t\\bar{t}}$ [GeV]"
+	p.Y.Label.Text = "$(1/\\sigma) \\: \\mathrm{d}\\sigma / \\mathrm{d}m_{t\\bar{t}}$"
+	p.X.Min, p.X.Max = -4, 6.0
+	p.Y.Min, p.Y.Max =  0, 0.5
+	applyPlotStyle(p) // contains all plot-related cosmetics
+	
+	// Create a histogram of our data values
+	hData := hplot.NewH1D(histData, hplot.WithYErrBars(true))
+	hData.Infos.Style = hplot.HInfoNone
+	hData.LineStyle.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 30}
+	hData.LineStyle.Width = 0
+	hData.YErrs.LineStyle.Color = color.NRGBA{R: 10, G: 10, B: 10, A: 255}
+	hData.YErrs.LineStyle.Width = 2.5
+	hData.YErrs.CapWidth = 8
+	hData.GlyphStyle = draw.GlyphStyle{
+		Shape:  draw.CircleGlyph{},
+		Color:  color.NRGBA{R: 10, G: 10, B: 10, A: 255},
+		Radius: vg.Points(3)}
+	p.Add(hData)
 
+	// Create and tune the histogram for simulation prediction
+	hMC := hplot.NewH1D(histMC)
+	hMC.Infos.Style = hplot.HInfoNone
+	hMC.LineStyle.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 30}
+	hMC.LineStyle.Width = 0
+	hMC.FillColor = color.NRGBA{R: 0, G: 0, B: 100, A: 30}
+	p.Add(hMC)
+	
+	// Tune the style of the model function (type hplot.NewFunction)
+	modelFunc.Samples = 1000
+	modelFunc.Color = color.NRGBA{R: 255, A: 255}
+	modelFunc.Width = vg.Points(2)
+	p.Add(modelFunc)
+	
+	// Add and tune the legend
+	p.Legend.Add("Data", hData)
+	p.Legend.Add("Simulation", hMC)
+	p.Legend.Add("Theory", modelFunc)
+	p.Legend.Top = true
+	p.Legend.Left = false
+	p.Legend.YOffs = 0
+	p.Legend.XOffs = -0.5 * vg.Inch
+	p.Legend.Padding = 0.1 * vg.Inch
+	p.Legend.ThumbnailWidth = 0.5 * vg.Inch
+
+	// Save the plot to a TEX file
+	if err := p.Save(6*vg.Inch, -1, "results/h1d_plot_wLatex.tex"); err != nil {
+		log.Fatalf("error saving plot: %v\n", err)
+	}
+	
+	// Save the plot to a PDF file (change labels without LaTeX)
+	p.Title.Text = "Gaussian Phenomena"
+	p.X.Label.Text = "x"
+	p.Y.Label.Text = "PDF(x)"
+	if err := p.Save(6*vg.Inch, -1, "results/h1d_plot_woLatex.pdf"); err != nil {
+		log.Fatalf("error saving plot: %v\n", err)
+	}
+
+	// Save the plot to a PNG
+	c := vgimg.NewWith(
+		vgimg.UseWH(12*vg.Centimeter, 9*vg.Centimeter),
+		vgimg.UseDPI(200),
+	)
+	dc := draw.New(c)
+	p.Draw(dc)
+	saveImg(c, "results/h1d_plot.png")
+}
+
+// Create fake data
+func getData() (*hbook.H1D, *hbook.H1D, *hplot.Function) {
+
+	const npoints = 500
+	
 	// Create a normal distribution
 	dist := distuv.Normal{
 		Mu:    0,
 		Sigma: 1,
 		Src:   rand.New(rand.NewSource(0)),
 	}
-
+	
 	// Draw some random values from the standard normal distribution
 	histData := hbook.NewH1D(20, -4, +4)
 	for i := 0; i < npoints; i++ {
 		v := dist.Rand()
 		histData.Fill(v, 1)
 	}
-
-	// normalize histogram
 	area := 0.0
 	for _, bin := range histData.Binning.Bins {
 		area += bin.SumW() * bin.XWidth()
@@ -68,135 +144,149 @@ func plotHplot_1D(){
 		v := dist.Rand()
 		histMC.Fill(v, 1)
 	}
-
-	// normalize histogram
 	area = 0.0
 	for _, bin := range histMC.Binning.Bins {
 		area += bin.SumW() * bin.XWidth()
 	}
 	histMC.Scale(1 / area)
-	
-	// Make a plot and set its title and axis range 
-	p := hplot.New()
 
-	// draw a grid which is produced by a function
-	// p.Add(newCustomGrid())
+	// Function correspondong to the theory prediction
+	modelFunc := hplot.NewFunction(dist.Prob)
 	
+	return histData, histMC, modelFunc
+}
+
+// Apply a nice plot style
+func applyPlotStyle(p *hplot.Plot){
+
 	// Plot borders --> doesn't work with LaTeX/PNG with vgimg yet
 	p.Border.Right = 15
 	p.Border.Left = 5
 	p.Border.Top = 10
 	p.Border.Bottom = 5
 
-	// Specify titles
-	p.Title.Text = "Gaussian Phenomena"
-	//p.Title.Text = "\\textbf{APLAS} Dummy -- $\\sqrt{s}=13\\,$TeV $\\mathcal{L}\\,=\\,3\\,$ab\\textsuperscript{-1}"
-	
 	// Specify title style
-	p.Title.TextStyle.Font.Size = 18
+	p.Title.TextStyle.Font.Size = 16
 	p.Title.TextStyle.Color = defaultBlack
 	p.Title.Padding = 10
 
 	// Specify axis ranges and padding (ie distance to (0,0) point
-	p.X.Min, p.X.Max = -4, 6
-	p.Y.Min, p.Y.Max =  0, 0.5
 	p.X.Padding = 5
 	p.Y.Padding = 5
 	
 	// Specify axis label & fontsize
-	//p.X.Label.Text = "$m_{t\\bar{t}}$ [GeV]"
-	//p.Y.Label.Text = "$(1/\\sigma) \\: \\mathrm{d}\\sigma / \\mathrm{d}m_{t\\bar{t}}$"
-	p.X.Label.Text = "X"
-	p.Y.Label.Text = "Y"
-	p.X.Label.TextStyle.Font.Size = 18
-	p.Y.Label.TextStyle.Font.Size = 18
+	p.X.Label.TextStyle.Font.Size = 14
+	p.Y.Label.TextStyle.Font.Size = 14
 	p.X.Label.TextStyle.Color = defaultBlack
 	p.Y.Label.TextStyle.Color = defaultBlack
 
-	// Specify axis line, ticks values, label style & size
-	p.X.LineStyle.Width = 1.1
-	p.Y.LineStyle.Width = 1.1
+	// Specify axis style
+	p.X.LineStyle.Width = 1.05
+	p.Y.LineStyle.Width = 1.05
 	p.X.LineStyle.Color = defaultBlack
 	p.Y.LineStyle.Color = defaultBlack
-	p.X.Tick.LineStyle.Width = 1.1
-	p.Y.Tick.LineStyle.Width = 1.1
+
+	// Specify ticks style
+	p.X.Tick.LineStyle.Width = 1.05
+	p.Y.Tick.LineStyle.Width = 1.05
 	p.X.Tick.LineStyle.Color = defaultBlack
 	p.Y.Tick.LineStyle.Color = defaultBlack
-	p.X.Tick.Marker = customTicks(p.X.Min, p.X.Max)
-	p.Y.Tick.Marker = customTicks(p.Y.Min, p.Y.Max)
-	p.X.Tick.Label.Font.Size = 14
-	p.Y.Tick.Label.Font.Size = 14
+	p.X.Tick.Label.Font.Size = 11
+	p.Y.Tick.Label.Font.Size = 11
 	p.X.Tick.Label.Color = defaultBlack
 	p.Y.Tick.Label.Color = defaultBlack
-	
-	// Create a histogram of our values drawn from the standard normal.
-	h := hplot.NewH1D(histData, hplot.WithYErrBars(true))
 
-	// Info of histogram 
-	h.Infos.Style = hplot.HInfoNone
+	// Specify tick position
+	// --> this is not good: if, say p.X.Min, is not set before, then it doesn't work.
+	//     In the meanwhile, add a protection.
+	//     Possible attempt with p.DataRanger?
+	if !math.IsInf(p.X.Min, 0) && !math.IsInf(p.Y.Max, 0) {
+		p.X.Tick.Marker = customTicks(p.X.Min, p.X.Max)
+		p.Y.Tick.Marker = customTicks(p.Y.Min, p.Y.Max)
+	}
 
-	// Line color / style
-	h.LineStyle.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 30}
-	h.LineStyle.Width = 0
-
-	// Error bar style
-	h.YErrs.LineStyle.Color = color.NRGBA{R: 10, G: 10, B: 10, A: 255}
-	h.YErrs.LineStyle.Width = 1.5
-
-	// Histo marker style / color
-	h.GlyphStyle = draw.GlyphStyle{
-		Shape:  draw.CircleGlyph{},
-		Color:  color.NRGBA{R: 10, G: 10, B: 10, A: 255},
-		Radius: vg.Points(3)}
-	p.Add(h)
-
-	
-	hMC := hplot.NewH1D(histMC)
-	hMC.Infos.Style = hplot.HInfoNone
-	hMC.LineStyle.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 30}
-	hMC.LineStyle.Width = 0
-	hMC.FillColor = color.NRGBA{R: 0, G: 0, B: 100, A: 30}
-	p.Add(hMC)
-	
-	// Add the normal distribution function with hplot.NewFunction 
-	norm := hplot.NewFunction(dist.Prob)
-	norm.Samples = 1000
-	norm.Color = color.NRGBA{R: 255, A: 255}
-	norm.Width = vg.Points(2)
-	p.Add(norm)
-	
-	// Add and manipulate a legend
-	p.Legend.Add("Data", h)
-	p.Legend.Add("Simulation", hMC)
-	p.Legend.Add("Theory", norm)
-	p.Legend.Top = true
-	p.Legend.Left = false
-	p.Legend.YOffs = 0
-	p.Legend.XOffs = -0.5 * vg.Inch
-	p.Legend.Padding = 0.1 * vg.Inch
-	p.Legend.ThumbnailWidth = 0.5 * vg.Inch
-	p.Legend.TextStyle.Font.Size = 15
+	// Specify text style of the legend
+	p.Legend.TextStyle.Font.Size = 14
 	p.Legend.TextStyle.Color = defaultBlack
 
-	// Save the plot to a TEX file
-	if err := p.Save(6*vg.Inch, -1, "h1d_plot_wLatex.tex"); err != nil {
-		log.Fatalf("error saving plot: %v\n", err)
+	// Add a grid if we want
+	// p.Add(newCustomGrid())
+
+}
+
+func saveImg(c *vgimg.Canvas, fname string) {
+	f, err := os.Create(fname)
+	if err != nil {
+		log.Fatalf("could not create output image file: %+v", err)
+	}
+	defer f.Close()
+
+	cpng := vgimg.PngCanvas{Canvas: c}
+	_, err = cpng.WriteTo(f)
+	if err != nil {
+		log.Fatalf("could not encode image to PNG: %+v", err)
 	}
 	
-	// Save the plot to a PDF file
-	if err := p.Save(6*vg.Inch, -1, "h1d_plot_woLatex.pdf"); err != nil {
-		log.Fatalf("error saving plot: %v\n", err)
+	err = f.Close()
+	if err != nil {
+		log.Fatalf("could not close output image file: %+v", err)
+	}
+}
+
+
+// Functions stolen from David Calvet:
+// https://gitlab.cern.ch/atlas-clermont/tile/front-end/fengo/-/blob/master/PlotHelpers.go
+func newCustomGrid() *plotter.Grid {
+	gr := plotter.NewGrid()
+	gr.Vertical.Color = color.Gray{220}
+	gr.Vertical.Dashes = []vg.Length{vg.Points(3), vg.Points(5)}
+	gr.Horizontal.Color = color.Gray{220}
+	gr.Horizontal.Dashes = []vg.Length{vg.Points(3), vg.Points(5)}
+	return gr
+}
+
+// Functions stolen from David Calvet:
+// https://gitlab.cern.ch/atlas-clermont/tile/front-end/fengo/-/blob/master/PlotHelpers.go
+func customTicks(ymin, ymax float64) plot.ConstantTicks {
+
+	ticks := []plot.Tick{}
+
+	// computing order of range (position of least significant digit)
+	yorder := int(math.Log10(ymax-ymin)+0.5) - 1
+	format := ".0f"
+	if yorder < 1 {
+		format = fmt.Sprintf(".%df", -yorder)
 	}
 
-	// Save the plot to a PNG (resolution doesnt work resolution - most likely not used properly)
-	c := vgimg.NewWith(
-		vgimg.UseWH(12*vg.Centimeter, 9*vg.Centimeter),
-		vgimg.UseDPI(200),
-	)
-	dc := draw.New(c)
-	p.Draw(dc)
-	saveImg(c, "h1d_plot.png")
+	// stepping is a power of 10 with integer exponent (yorder)
+	ystep := math.Pow10(yorder)
+
+	// tuning step
+	if (ymax-ymin)/ystep > 20 {
+		ystep *= 5
+	}
+
+	// first big tick is rounded to the correct significant digit
+	yoffset := float64(int(ymin/ystep)) * ystep
+
+	// creating big ticks
+	for y := yoffset; y <= ymax; y += ystep {
+		label := fmt.Sprintf("%"+format, y)
+		ticks = append(ticks, plot.Tick{y, label})
+	}
+
+	// 5 small ticks for each big tick
+	ysub := ystep / 5
+	for y := yoffset - ysub; y >= ymin; y -= ysub {
+		ticks = append(ticks, plot.Tick{y, ""})
+	}
+
+	for y := yoffset + ysub; y <= ymax; y += ysub {
+		ticks = append(ticks, plot.Tick{y, ""})
+	}
+	return plot.ConstantTicks(ticks)
 }
+
 
 // Use plotUtil package
 func createPlotUtil(){
@@ -232,7 +322,7 @@ func createPlotUtil(){
 	p.Legend.Left = true
 
 	// Save the plot to a PDF file.
-	err = p.Save(4*vg.Inch, 4*vg.Inch, "points.pdf")
+	err = p.Save(4*vg.Inch, 4*vg.Inch, "results/points.pdf")
 	if err != nil {
 		log.Fatalf("could not save plot: %+v", err)
 	}
@@ -248,67 +338,3 @@ func getPoints(xs []float64, f func(float64) float64) plotter.XYs {
 	return pts
 }
 
-
-func saveImg(c *vgimg.Canvas, fname string) {
-	f, err := os.Create(fname)
-	if err != nil {
-		log.Fatalf("could not create output image file: %+v", err)
-	}
-	defer f.Close()
-
-	cpng := vgimg.PngCanvas{Canvas: c}
-	_, err = cpng.WriteTo(f)
-	if err != nil {
-		log.Fatalf("could not encode image to PNG: %+v", err)
-	}
-	
-	err = f.Close()
-	if err != nil {
-		log.Fatalf("could not close output image file: %+v", err)
-	}
-}
-
-
-// Functions stolen from David Calvet:
-// https://gitlab.cern.ch/atlas-clermont/tile/front-end/fengo/-/blob/master/PlotHelpers.go
-func newCustomGrid() *plotter.Grid {
-	gr := plotter.NewGrid()
-	gr.Vertical.Color = color.Gray{220}
-	gr.Vertical.Dashes = []vg.Length{vg.Points(3), vg.Points(5)}
-	gr.Horizontal.Color = color.Gray{220}
-	gr.Horizontal.Dashes = []vg.Length{vg.Points(3), vg.Points(5)}
-	return gr
-}
-
-func customTicks(ymin, ymax float64) plot.ConstantTicks {
-
-	ticks := []plot.Tick{}
-	// computing order of range (position of least significant digit)
-	yorder := int(math.Log10(ymax-ymin)+0.5) - 1
-	format := ".0f"
-	if yorder < 1 {
-		format = fmt.Sprintf(".%df", -yorder)
-	}
-	// stepping is a power of 10 with integer exponent (yorder)
-	ystep := math.Pow10(yorder)
-	// tuning step
-	if (ymax-ymin)/ystep > 20 {
-		ystep *= 5
-	}
-	// first big tick is rounded to the correct significant digit
-	yoffset := float64(int(ymin/ystep)) * ystep
-	// creating big ticks
-	for y := yoffset; y <= ymax; y += ystep {
-		label := fmt.Sprintf("%"+format, y)
-		ticks = append(ticks, plot.Tick{y, label})
-	}
-	// 5 small ticks for each big tick
-	ysub := ystep / 5
-	for y := yoffset - ysub; y >= ymin; y -= ysub {
-		ticks = append(ticks, plot.Tick{y, ""})
-	}
-	for y := yoffset + ysub; y <= ymax; y += ysub {
-		ticks = append(ticks, plot.Tick{y, ""})
-	}
-	return plot.ConstantTicks(ticks)
-}
