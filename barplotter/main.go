dc2ee9a0564f81bd82aad17316aeed5b1c350f8f
rmadar <romain.madar@gmail.com>
2020-06-13 15:17:05 +0200

    First working example

--- barplotter/main.go
+++ barplotter/main.go
@@ -3,8 +3,13 @@
 package main
 
 import (
-	"fmt"
-	
+	_ "fmt"
+	"log"
+	"image/color"
+
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 )
 
@@ -16,26 +21,82 @@ type BinnedBars struct {
 	
 	// Height of bars, ie float for every bins
 	plotter.Values
+
+	// Color of bars
+	Color color.Color
 }
 
 // Trying the new plotter
 func main() {
 
-	// Test binning
-	bins := NewBinning(10, 0, 10)
-	fmt.Println(bins)
+	// Binned
+	vals := []float64{0.4, 0.5, 0.35, 0.67, 0.80, 0.9, 1}
+	bs := NewBinnedBars(vals, NewBinning(len(vals), 0, 10) )
+	bs.Color = color.RGBA{R:196, B:128, A: 255}
 
+	// Plot
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Binned Bar Chart"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	p.Add(bs)
+
+	// Range
+	p.X.Min = 0
+	p.X.Max = 10
+	p.Y.Min = 0
+	p.Y.Max = 1.2
 	
+	// Save the plot
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "BinnedBars.png"); err != nil {
+		panic(err)
+	}
 }
 
-// 
-func NewBinnedBars() BinnedBars {
+// NewBinnedBars creates a new binned bar plot
+func NewBinnedBars(vals []float64, bins Binning) *BinnedBars {
+	cpy, err := plotter.CopyValues( (plotter.Values)(vals) )
+	if err != nil {
+		log.Fatalf("cannot copy values")
+	}
 	
+	return &BinnedBars{
+		Binning: bins,
+		Values:  cpy,
+	}
 }
 
-// Cheap binning type - to start with something
+// Plot implements the Plotter interface, drawing a rectangle
+// defined by (xLow, xHigh) and Heights.
+func (bs *BinnedBars) Plot(c draw.Canvas, plt *plot.Plot) {
+
+	// Get coordinate transformation
+	trX, trY := plt.Transforms(&c)
+
+	// Loop over values
+	for i, v := range bs.Values {
+
+		xlo := trX(bs.Binning[i][0])
+		xhi := trX(bs.Binning[i][1])
+		val := trY(v)
+
+		var pts []vg.Point
+		pts = append(pts, vg.Point{X: xlo, Y:   0})
+		pts = append(pts, vg.Point{X: xlo, Y: val})
+		pts = append(pts, vg.Point{X: xhi, Y: val})
+		pts = append(pts, vg.Point{X: xhi, Y:   0})
+
+		c.FillPolygon(bs.Color, c.ClipPolygonXY(pts))
+	}
+}
+
+// Simple binning type
 type Binning [][2]float64
 
+// NewBinning creates a new equal binning
 func NewBinning(n int, xmin, xmax float64) Binning {
 	res := make([][2]float64, n)
 	dx := (xmax - xmin) / float64(n)
