aff1fe6b9b4f72ca040e04595e87a87292b22580
rmadar <romain.madar@gmail.com>
2022-01-31 15:17:14 +0100

    Update the reading tree part with up-to-date packages

--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module github.com/rmadar/go-simple-examples
 
-go 1.14
+go 1.16
 
 // For local tests during dev
 // replace github.com/rmadar/go-lorentz-vector => /home/rmadar/cernbox/goDev/go-lorentz-vector
 // replace go-hep.org/x/hep => /home/rmadar/cernbox/goDev/gohep_dev/hep
 
-require go-hep.org/x/hep v0.28.4 // indirect
+require (
+	go-hep.org/x/hep v0.30.1
+	gonum.org/v1/gonum v0.9.3
+)
--- reading-root-ttree/main.go
+++ reading-root-ttree/main.go
@@ -11,8 +11,8 @@ import (
 	"go-hep.org/x/hep/groot"
 	"go-hep.org/x/hep/groot/rtree"
 
-	"github.com/golang/geo/r3"
-	"github.com/rmadar/go-lorentz-vector/lv"
+	"gonum.org/v1/gonum/spatial/r3"
+	"go-hep.org/x/hep/fmom"
 )
 
 type Event struct {
@@ -71,11 +71,11 @@ func eventLoop(fname string, tname string, evtmax int64, verbose bool) {
 		e Event
 		rvars = getReadVariables(&e)
 	)
-	sc, err := rtree.NewScannerVars(tree, rvars...)
+	r, err := rtree.NewReader(tree, rvars)
 	if err != nil {
-		log.Fatalf("could not create scanner: %+v", err)
+		log.Fatalf("could not create tree reader: %+v", err)
 	}
-	defer sc.Close()
+	defer r.Close()
 	
 	// Ceate a new file, new writer to save new variables in a tree
 	fnameOut := strings.ReplaceAll(fname, ".root", "_processed.root")
@@ -104,16 +104,10 @@ func eventLoop(fname string, tname string, evtmax int64, verbose bool) {
 	defer tout.Close()
 
 	// Actual event loop
-	for sc.Next() && sc.Entry() < evtmax {
+	err = r.Read(func(ctx rtree.RCtx) error {
 
 		// Entry index
-		ievt := sc.Entry()
-
-		// Load variable of the event
-		err := sc.Scan()
-		if err != nil {
-			log.Fatalf("could not scan entry %d: %+v", ievt, err)
-		}
+		ievt := ctx.Entry
 
 		// Print the partonic event
 		if ievt%100 == 0 && verbose {
@@ -125,16 +119,19 @@ func eventLoop(fname string, tname string, evtmax int64, verbose bool) {
 		var (
 			tops = e.t
 			leptons = e.l
-			tplus_P4, tminus_P4 lv.FourVec
-			lplus_P4, lminus_P4 lv.FourVec
+			tplus_P4, tminus_P4 fmom.PxPyPzE
+			lplus_P4, lminus_P4 fmom.PxPyPzE
 		)
 		// Get top/anti-top and lepton/ant-lepton four-vectors
-		get4Vec := func(parts Particles, i int32) lv.FourVec {
-			return lv.NewFourVecPtEtaPhiM(
+		get4Vec := func(parts Particles, i int32) fmom.PxPyPzE {
+			var p fmom.PxPyPzE
+			p.SetPtEtaPhiM(
 				float64(parts.pt[i]),
 				float64(parts.eta[i]),
 				float64(parts.phi[i]),
-				float64(parts.m[i])) 
+				float64(parts.m[i]),
+			)
+			return p
 		}		
 		if tops.pid[0]>0 {
 			tplus_P4  = get4Vec(tops, 0)
@@ -158,83 +155,99 @@ func eventLoop(fname string, tname string, evtmax int64, verbose bool) {
 		spin_var.kVec = [3]float64{k.X, k.Y, k.Z}
 		spin_var.rVec = [3]float64{r.X, r.Y, r.Z}
 		spin_var.nVec = [3]float64{n.X, n.Y, n.Z}
-		spin_var.dphi_ll = lplus_P4.DeltaPhi(lminus_P4)
+		spin_var.dphi_ll = cosTheta["dphi"]
 		spin_var.cos_km = cosTheta["k-"]
 		spin_var.cos_rm = cosTheta["r-"]
 		spin_var.cos_nm = cosTheta["n-"]
 		spin_var.cos_kp = cosTheta["k+"]
 		spin_var.cos_rp = cosTheta["r+"]
 		spin_var.cos_np = cosTheta["n+"]
-
+		
 		_, err = tout.Write()
 		if err != nil {
 			log.Fatalf("could not write event %d: %+v", ievt, err)
 		}
-	}
-
+			
+		return nil
+	})
+		
 	err = tout.Close()
 	if err != nil {
 		log.Fatalf("could not close tree-writer: %+v", err)
 	}
 	
-	fmt.Println(" --> Event loop is done:", sc.Entry(), "events processed and stored in", fnameOut)
+	fmt.Println(" --> Event loop is done:", tree.Entries(), "events processed and stored in", fnameOut)
 }
 
 // Compute spin-related cosines
-func computeSpinCosines(tplus, tminus, lplus, lminus lv.FourVec) map[string]float64 {
+func computeSpinCosines(tplus, tminus, lplus, lminus fmom.PxPyPzE) map[string]float64 {
 
 	// Get the proper basis
 	k, r, n := getSpinBasis(tplus, tminus)
+	
+	// 1. Get ttbar boost
+	ttbar        := fmom.Add(&tplus, &tminus)
+	boost_ttbar  := fmom.BoostOf(ttbar).Scale(-1)
+	
+	// 2. Move tops and leptons in ttbar rest-frame
+	tplus_ttbar  := fmom.Boost(&tplus , boost_ttbar) 
+	tminus_ttbar := fmom.Boost(&tminus, boost_ttbar) 
+	lplus_ttbar  := fmom.Boost(&lplus , boost_ttbar) 	
+	lminus_ttbar := fmom.Boost(&lminus, boost_ttbar) 
 
-	// Get 3-vectors of lplus (lminus) in tplus (tmius) rest-frame
-	lplusRF := lplus.ToRestFrameOf(tplus).Pvec
-	lminusRF := lminus.ToRestFrameOf(tminus).Pvec
-
+	// 3. Move leptons in their top parent rest-frame, from the ttbar rest-frame
+	lplusRF4m  := fmom.Boost(lplus_ttbar , fmom.BoostOf(tplus_ttbar).Scale(-1))
+	lminusRF4m := fmom.Boost(lminus_ttbar, fmom.BoostOf(tminus_ttbar).Scale(-1))
+	lplusRF    := fmom.VecOf(lplusRF4m)
+	lminusRF   := fmom.VecOf(lminusRF4m)
+	
 	// Fill the six cosines
-	getCos := func(a, b r3.Vector, m float64) float64 {
-		return math.Cos(a.Angle(b.Mul(m)).Radians())
+	getCos := func(a, b r3.Vec, m float64) float64 {
+		return a.Dot(b.Scale(m)) / (r3.Norm(a) * r3.Norm(b.Scale(m)))
 	}
 	cosTheta := map[string]float64{
-		"k+": getCos(lplusRF, k, 1),
-		"r+": getCos(lplusRF, r, 1),
-		"n+": getCos(lplusRF, n, 1),
-		"k-": getCos(lminusRF, k, -1),
-		"r-": getCos(lminusRF, r, -1),
-		"n-": getCos(lminusRF, n, -1),
+		"k+"  : getCos(lplusRF, k, 1),
+		"r+"  : getCos(lplusRF, r, 1),
+		"n+"  : getCos(lplusRF, n, 1),
+		"k-"  : getCos(lminusRF, k, -1),
+		"r-"  : getCos(lminusRF, r, -1),
+		"n-"  : getCos(lminusRF, n, -1),
+		"dphi": getCos(lplusRF, lminusRF, 1),
 	}
 
 	return cosTheta
 }
 
 // Get spin basis
-func getSpinBasis(t, tbar lv.FourVec) (k, r, n r3.Vector) {
+func getSpinBasis(t, tbar fmom.PxPyPzE) (k, r, n r3.Vec) {
 
 	// Get top direction in ttbar rest frame
-	ttbar := t.Add(tbar)
-	top_rest := t.ToRestFrameOf(ttbar)
-	k = top_rest.Pvec.Normalize()
+	ttbar := fmom.Add(&t, &tbar)
+	top_rest := fmom.Boost(&t, fmom.BoostOf(ttbar).Scale(-1))
+	top_rest_pvec := fmom.VecOf(top_rest)
+	k = top_rest_pvec.Scale(1 / r3.Norm(top_rest_pvec))
 
 	// Get the beam axis (Oz) and coeff to build ortho-normal basis
-	beam_axis := r3.Vector{0, 0, 1}
+	beam_axis := r3.Vec{0, 0, 1}
 	yval := k.Dot(beam_axis)
 	ysign := yval / math.Abs(yval)
 	rval := math.Sqrt(1 - yval*yval)
 
 	// Get r axis: r = sign(y)/r * (beam -y*k)
-	r = beam_axis.Add(k.Mul(-yval))
-	r = r.Mul(1. / rval * ysign)
+	r = beam_axis.Add(k.Scale(-yval))
+	r = r.Scale(1. / rval * ysign)
 
 	// Get n axis: n = sign(y)/r * (beam cross k)
 	n = beam_axis.Cross(k)
-	n = n.Mul(1. / rval * ysign)
+	n = n.Scale(1. / rval * ysign)
 
 	return k, r, n
 }
 
 
 // Helper to define the event variables to load
-func getReadVariables(e *Event) []rtree.ScanVar {
-	return []rtree.ScanVar{
+func getReadVariables(e *Event) []rtree.ReadVar {
+	return []rtree.ReadVar{
 		// Top-quarks
 		{Name: "t_pt", Value: &e.t.pt},
 		{Name: "t_eta", Value: &e.t.eta},
